b91b503975519260b1d351b6e49b2449f59d615f
Tiffany W <t.washington0008@gmail.com>
2023-01-21 19:37:59 -0800

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 //will act as a client for the Mongo server, requires mongodb node driver, and from this the mongoclient object (we requiered from it)
-const assert = require('assert').strict;
+// Don't need below since promises do their own error handling
+// const assert = require('assert').strict;
 const dboper = require('./operations');
 //dboper is short for db operation, we are using it to get access to the methods under opertaions.js
 
@@ -11,10 +12,11 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'campsite';
 
 //to access the server using these configs- must connect
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 //above the MongoClient.connect has these arguments, and the last one is a CB fx with the parameters err onj and client (and obj which we will use to access the db on the const db=client.db line) and returns this
     //assert allows you to perform various checks     
-    assert.strictEqual(err, null);
+    // assert.strictEqual(err, null);
     //if err ==== null then this assert will fail and throw an error and terminate the entire applciation; if not (so err does have a null value), it will move on
 
     console.log('Connected correctly to server (MongoDB)');
@@ -26,43 +28,58 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     //you wouldn't do this normally, but this helps you so you don't have leftover docs from testing while learning
 
     //we use DROP in DB's versus DELETE
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
+    // db.dropCollection('campsites', (err, result) => {
+        db.dropCollection('campsites')
+        .then(result => {
+        // assert.strictEqual(err, null);
         //you will assert that the error is not null, if that is true then we will continue on
         console.log('Dropped Collection', result);
-
+        })
+        .catch(err => console.log('No collection to drop.'));
         //**REMOVED BELOW SINCE NOW DOING THRU DBOPER module above **then recreate campsites and get access to it by setting const collection to below
         // const collection = db.collection('campsites');
 
         // now will insert a doc
         // collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
         //we are CALLING dboper
-        dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 
+        dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites')
         //we are defining this fx
-        'campsites', result => {
-        
+        .then(result => {
         // (err, result) => {
         //     assert.strictEqual(err, null);
             console.log('Insert Document:', result.ops);
             // //ops is a property short for operations, depending on the method it can contain diff values, for insert.one it will contain an array of the doc that was inserted
-            dboper.findDocuments(db, 'campsites', docs => {
+                return dboper.findDocuments(db, 'campsites'); 
+            })
+            .then(docs => {
                 console.log('Found Documents:', docs);
             
-            dboper.updateDocument(db, { name: 'Breadcrumb Trail Campground' }, 
-            { description: "Updated Test Description"}, 'campsites', 
-            result => {
+            return dboper.updateDocument(db, { name: 'Breadcrumb Trail Campground' }, 
+                { description: "Updated Test Description"}, 'campsites'); 
+            })
+            .then(result => {
                 console.log('Updated Document Count:',  result.result.nModified);
 
-                dboper.findDocuments(db, 'campsites', docs => {
-                    console.log('Found Documents:', docs);
+                return dboper.findDocuments(db, 'campsites');
+            })
+            .then(docs => {
+                console.log('Found Documents:', docs);
 
-                    dboper.removeDocument(db, { name: "Breadcrumb Trail Campground"},
-                    'campsites', result => {
-                        console.log('Deleted Document Count:', result.deletedCount);
-                        client.close();
-                    });
-                });
+                return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground"},
+                'campsites');
+            })
+            .then(result => {
+                 console.log('Deleted Document Count:', result.deletedCount);
+                return client.close();
+                //returning the close as a promise as well so if any error in closing the client, it will be handled as a part of this chain
+            })
+            //adding a catch to handle any errors in the chain.  In a promise chain, if any errors resulting in a promise being rejected, it will go here
+            .catch(err => {
+                console.log(err);
+                client.close();
+                //adding another since an error coul dhappen before the other client.close method is called
             });
+             
             //we are sking it to look for the doc "Breadcrump Trail".. and then update it with updated test descrip.
             
             
@@ -74,11 +91,8 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
                 // client.close();
             //     //above will then close the client's connection to the db server
-            });
-            
-        });
-    });
-
-});
+         
+})
+.catch(err => console.log(err));
 
 //we're using CB fx's like this bc we're using asynchronous ops, when we're communicating between the node app here and the MongoDB server, it's not async.  It takes time for that comm to happen, no matter how small.  But the CB nesting we are doing isn't necessarily what we want to do but we'll talk about it later
--- operations.js
+++ operations.js
@@ -1,46 +1,60 @@
-const assert = require('assert').strict;
+// const assert = require('assert').strict;
+//**No longer need above since using promise-based approach */
 //4 methods to insert, find, remove, update docs and then export each
-exports.insertDocument = (db, document, collection, callback) => {
+// exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
     //expecting that the collection argument passed in will be a string like campsites and then use it to obtain a ref to the collection named campsites, so we can use the coll const coll to interact with a collection
 
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-        //the above is a CB that is defined elsewhere where a call to this insert document is made, and we are delivering the result to this CB to do whatever it is called to do
-    });
+    //*** REPLACING WITH PROMISED BASED APPROACH, removed callback from the argument list */
+    // coll.insertOne(document, (err, result) => {
+    //     assert.strictEqual(err, null);
+    //     callback(result);
+    //     //the above is a CB that is defined elsewhere where a call to this insert document is made, and we are delivering the result to this CB to do whatever it is called to do
+    // });
     //these methods are a part of the mongoDB API, go to the doc for details
+
+    return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+// exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
 
-    coll.find().toArray((err, docs) => {
-        //empty find means we want to find all docs in this collection, then use toArray to put them into an Array
-        assert.strictEqual(err, null);
-        //if no error, so err===null then we will do the CB
-        callback(docs);
-    });
+    //*** REPLACING WITH PROMISED BASED APPROACH, removed callback from the argument list */
+    // coll.find().toArray((err, docs) => {
+    //     //empty find means we want to find all docs in this collection, then use toArray to put them into an Array
+    //     assert.strictEqual(err, null);
+    //     //if no error, so err===null then we will do the CB
+    //     callback(docs);
+    // });
+
+    return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+// exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
 
-    coll.deleteOne(document, (err, result) => {
-        //the CB will have the err and result params to check the err with an assert
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    //*** REPLACING WITH PROMISED BASED APPROACH, removed callback from the argument list */
+    // coll.deleteOne(document, (err, result) => {
+    //     //the CB will have the err and result params to check the err with an assert
+    //     assert.strictEqual(err, null);
+    //     callback(result);
+    // });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+// exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
 
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        //using an update operator to pass to mongodb $set; null for third is for optional configs since we don't need it
+        //*** REPLACING WITH PROMISED BASED APPROACH, removed callback from the argument list */
+    // coll.updateOne(document, { $set: update }, null, (err, result) => {
+    //     //using an update operator to pass to mongodb $set; null for third is for optional configs since we don't need it
     
-    assert.strictEqual(err, null);
-    callback(result);
-    });
-    
+    // assert.strictEqual(err, null);
+    // callback(result);
+    // });
+    return coll.updateOne(document, { $set: update }, null);
 };
